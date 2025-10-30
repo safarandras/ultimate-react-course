041ab79c12b328f8cd68a50bc9cac6eae46763f2
safarandras <safarandras00@gmail.com>
2025-10-30 11:05:20 +0100

    Workout Timer

---
+++ 13-workout-timer/starter/src/Clock.js
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+
+// function Clock() {
+//   const [time, setTime] = useState(formatTime(new Date()));
+
+//   return <time>For your workout on {time}</time>;
+// }
+
+// export default Clock;
